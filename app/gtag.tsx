239e93862523d4151043175d2b7a54101f47e4d1
Manuel Gegenhuber <Manuel.gegenhuber.mg@gmail.com>
2023-09-30 14:22:53 +0200

    fix: change id -> event_id

--- app/gtag.tsx
+++ app/gtag.tsx
@@ -1,68 +1,68 @@
-export const GA_TRACKING_ID = "G-VL811QC7WC" //replace it with your measurement id
+export const GA_TRACKING_event_id = "G-VL811QC7WC" //replace it with your measurement event_id
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+// https://developers.google.com/analytics/devguevent_ides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  window.gtag("config", GA_TRACKING_ID, {
+  window.gtag("config", GA_TRACKING_event_id, {
     page_path: url,
   })
 }
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+// https://developers.google.com/analytics/devguevent_ides/collection/gtagjs/events
 
 export default {
   pageview,
   events: {
     fuqwqs: (params: { sku: string, product_name: string, price: string, quantity: number, variation_type: string }) => {
       window.gtag("event", 'click', {
-        id: 'fuqwqs',
+        event_id: 'fuqwqs',
         element: 'remove_product',
         ...params
       });
     },
     icicqg: (params: { sku: string, product_name: string, price: string }) => {
       window.gtag("event", 'click', {
-        id: 'icicqg',
+        event_id: 'icicqg',
         element: 'new_products',
         ...params
       });
     },
     fhuhpd: (params: { sku: string, product_name: string, price: string }) => {
       window.gtag("event", 'click', {
-        id: 'fhuhpd',
+        event_id: 'fhuhpd',
         element: 'best_sellers',
         ...params
       });
     },
     pdsebd: () => {
       window.gtag("event", 'click', {
-        id: 'pdsebd',
+        event_id: 'pdsebd',
         element: 'open_cart',
       });
     },
     staraz: (params: { collection_type: string }) => {
       window.gtag("event", 'collection', {
-        id: 'staraz',
+        event_id: 'staraz',
         element: 'collection',
         ...params
       });
     },
     ijzndb: (params: { sort_by_type: string }) => {
       window.gtag("event", 'sort_by', {
-        id: 'ijzndb',
+        event_id: 'ijzndb',
         element: 'sort_by',
         ...params
       });
     },
     przfbb: (params: { search_text: string, sku: string, product_name: string, price: string }) => {
       window.gtag("event", 'click', {
-        id: 'przfbb',
+        event_id: 'przfbb',
         element: 'product_card',
         ...params
       });
     },
     davvny: () => {
       window.gtag("event", 'click', {
-        id: 'davvny',
+        event_id: 'davvny',
         element: 'search_bar',
       });
     },
@@ -71,34 +71,34 @@ export default {
     },
     mkspxa: (params: { sku: string, product_name: string, price: string }) => {
       window.gtag("event", 'click', {
-        id: 'mkspxa',
+        event_id: 'mkspxa',
         element: 'related_products',
         ...params
       });
     },
     xjgsbj: () => {
       window.gtag("event", 'click', {
-        id: 'xjgsbj',
+        event_id: 'xjgsbj',
         element: 'close_cart',
       });
     },
     ixtwas: (params: { sku: string, product_name: string, price: string, variation_type: string }) => {
       window.gtag("event", 'click', {
-        id: 'ixtwas',
+        event_id: 'ixtwas',
         element: 'add_quantity',
         ...params
       });
     },
     ijjuls: (params: { sku: string, product_name: string, price: string, variation_type: string }) => {
       window.gtag("event", 'click', {
-        id: 'ijjuls',
+        event_id: 'ijjuls',
         element: 'remove_quantity',
         ...params
       });
     },
     bczmbw: (params: { list_size: number, price: string, taxes: string }) => {
       window.gtag("event", 'click', {
-        id: 'bczmbw',
+        event_id: 'bczmbw',
         element: 'proceed_to_checkout',
         ...params
       });
